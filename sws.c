091a9479041e1912b3df1d0342c603e9d32a7d71
Ben Iofel <biofel@stevens.edu>
2018-11-01 20:30:17 -0400

    reuse socket even if code crashed

--- sws.c
+++ sws.c
@@ -15,6 +15,9 @@ int main() {
     struct sockaddr_in server, client;
     int serversock, clientsock;
     socklen_t clientsz;
+    int on;
+
+    on = 1;
 
     serversock = socket(PF_INET, SOCK_STREAM, 0);
     if (serversock < 0)
@@ -25,6 +28,9 @@ int main() {
         .sin_addr.s_addr = INADDR_ANY,
         .sin_port = htons(PORT)
     };
+
+    if (setsockopt(serversock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof on) < 0)
+        perror("setsockopt(SO_REUSEADDR)");
 
     if (bind(serversock, (struct sockaddr *)&server, sizeof(server)) < 0)
         err(1, "bind(port = %d)", PORT);
